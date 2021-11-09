4030be37c810dee4d1e860f00eeed4b44fa20d96
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2021-11-09 14:36:01 -0600

    added background, gravity, and click space a lot to fly

--- source.js
+++ source.js
@@ -1,65 +1,37 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-let spaceship =
-{
-    color: "black",
-    width: 8,
-    height: 22,
-    position:
-    {
-        x: 0,
-        y: 0
-    },
-    angle: 0,
-    engineOn: false,
-    rotatingLeft: false,
-    rotatingRight: false
-}
+document.addEventListener('DOMContentLoaded', () =>{
+    const spaceship =  document.querySelector('.spaceship');
+    const gameDisplay = document.querySelector('.container');
+    const ground = document.querySelector('ground');
+    const projectile = document.querySelector('.projectile');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+    let spaceshipLeft = 220;
+    let spaceshipBottom = 100;
+    let gravity = 2;
+    let thrust = 10;
+    let projectileBottom = 220;
+    let projectileLeft = 100;
 
-console.log('hello user');
 
-class Spaceship {
-    constructor (x , y , color , angle , engineOn , rotatingLeft , rotatingRight) {
-        this.x
-        this.y
-        this.radius = radius;
-        this.color = color;
-        this.angle= 0;
-        this.engineOn= false;
-        this.rotatingLeft= false;
-        this.rotatingRight= false;
+function startGame() {
+    spaceship.style.bottom = spaceshipBottom + 'px';
+    spaceship.style.left = spaceshipLeft + 'px';
+    spaceshipBottom -= gravity;
+    projectile.style.bottom = projectileBottom + 'px';
+    projectile.style.left =  projectileLeft + 'px';
     }
-    
- drawSpaceship()
-{
-    context.save();
-    context.beginPath();
-    context.translate(spaceship.width , spaceship.height);
-    context.rotate(spaceship.angle);
-    context.rect(spaceship.width * -0.5, spaceship.height * -0.5, spaceship.width, spaceship.height);
-    context.fillStyle = spaceship.color;
-    context.fill();
-    context.closePath();
+ let timerID = setInterval(startGame, 20);
 
-    // Draw the flame if engine is on
-    if(spaceship.engineOn)
-    {
-        context.beginPath();
-        context.moveTo(spaceship.width * -0.5, spaceship.height * 0.5);
-        context.lineTo(spaceship.width * 0.5, spaceship.height * 0.5);
-        context.lineTo(0, spaceship.height * 0.5 + Math.random() * 5);
-        context.lineTo(spaceship.width * -0.5, spaceship.height * 0.5);
-        context.closePath();
-        context.fillStyle = "orange";
-        context.fill();
-    }
-    context.restore();
+
+function jump(){
+    spaceshipBottom += 50;
+    spaceship.style.bottom = spaceshipBottom + 'px';
 }
-}
+addEventListener('keyup', jump);
+
+})
 
 
 
-
+//document.addEventListener('click', function(){
+    //console.log('hello world');
+//})
