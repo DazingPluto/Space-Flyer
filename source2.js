9f31bdb80eea0ba5009b2417c20880f6809d0994
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2021-11-10 16:17:34 -0600

    getting it donegit add .git add .

--- source2.js
+++ source2.js
@@ -3,17 +3,17 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1000;
 canvas.height = 800;
 
-const key = [] /// holds all of our keys for our key functions
+const keys = []; /// holds all of our keys for our key functions
 
 const player={
-    x:0,
-    y:0,
+    x:200,
+    y:200,
     width: 32,
     height: 44.5,
     frameX: 0,
     frameY:0,
-    //gravity:y - 6,
-    speed: 9,
+    gravity:- 6,
+    speed: 20,
     moving: false,
 }
 const playerSrite = new Image ();
@@ -26,11 +26,36 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 function animateGame(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(playerSrite, 0, 0, player.width, player.height, 200, 200, player.width, player.height);
+    drawSprite(playerSrite, 0, 0, player.width, player.height, player.x, player.y, player.width, player.height);
     requestAnimationFrame(animateGame);
+    movePlayer();
+    gravity();
 }
 animateGame();
-
+window.addEventListener("keydown", function(e){
+    keys[e.keyCode] = true;
+    console.log(keys);
+});
+window.addEventListener("keyup", function(e){
+    delete keys[e.keyCode];
+});
+function movePlayer (){
+    if(keys[38] && player.y > 20){
+        player.y -= player.speed;
+    }
+}
+function gravity(){
+    if(player.y < 750){
+    player.y += 6;
+    }
+}
+window.addEventListener('click', function(event){
+    //console.log(event);
+    //console.log('click works!');
+    const projectile = new Projectile(
+        event.clientY, event.clientX, 100, 'red', null);
+        projectile.draw();
+})
 
 
 
