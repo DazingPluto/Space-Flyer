025a475eb39802ba104494e71d495ac87ad78bf3
DazingPluto <92892528+DazingPluto@users.noreply.github.com>
2021-11-15 09:08:17 -0600

    :)

--- source.js
+++ source.js
@@ -134,13 +134,13 @@ function movementHandler(e) {
          
          
       const velocity = {
-             x: Math.cos(angle) * 8  ,          //here is where i set my x and y velocity to be always centered, that way all of my projectiles move in a baracading mannor
-             y: Math.sin(angle) * 8 
+             x: Math.cos(angle) * 6 ,          //here is where i set my x and y velocity to be always centered, that way all of my projectiles move in a baracading mannor
+             y: Math.sin(angle) * 6 
          }
            //console.log(velocity);
          enemies.push(new Enemy(x, y, radius, color, velocity));   // <---------here velocity is called, and new enemies are created with all of the data provided above, Then PUSHed to the end of enemies array
          console.log(enemies);          //<-------this allows me to keep track of enemies and collision, to make sure everything is always looping and working.
-    }, 5000)
+    }, 1000)
  }
 let animateOff
 let score = 0;
